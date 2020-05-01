31fa8d58529754d104e8221ad353159fa587c638
Andronikus <tiagoafroque@gmail.com>
2020-05-01 23:55:22 +0100

    first commit of covid info app

---
+++ src/components/chart/chart.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './chart.module.css';
+
+const Chart = () => {
+
+    return (
+        <h2>Chart</h2>
+    );
+}
+
+export default Chart;
---
+++ src/components/country-picker/country-picker.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './country-picker.module.css';
+
+const CountryPicker = () => {
+
+    return (
+        <h2>CountryPicker</h2>
+    );
+}
+
+export default CountryPicker;
